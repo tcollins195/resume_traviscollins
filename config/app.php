fb89351fba054b30e22d36dbb9d13e1b4f7c94e7
unknown <tcollins195@wit-xdvm-029.MILLARDPS.NET>
2013-03-05 13:16:42 -0600

    Initial commit

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+// Default page
+define('DEFAULT_PAGE','list_contacts');
+
+
+// Define navigation pages
+$pages = array(
+	'list_contacts' 	=> 'Contacts',
+	'list_groups'		=> 'Groups',
+	'Add'				=> 	array(
+								'form_add_contact' 	=> 'New Contact',
+								'form_add_group' 	=> 'New Group' 	
+							)
+);
---
+++ config/db.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_NAME', 'mycontacts');
+define('DB_USER', 'mycontacts_user');
+define('DB_PASS', 's9D8HZJ3T8QBtuRZ');
